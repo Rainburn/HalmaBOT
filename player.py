5d7aaaf56c5183eb1995676191bb347160984e38
Rainburn <cyber221168@yahoo.com>
2020-10-19 14:31:21 +0700

    changing player.py

--- player.py
+++ player.py
@@ -1,14 +1,18 @@
 class Player :
 
     def __init__(self, color):
-        self.player_color = color
         if (color == "RED") or (color == "R"):
+            self.player_color = color
             self.player_turn = 1
         else :
+            self.player_color = "GREEN"
             self.player_turn = 2
 
     def getTurn(self) :
         return self.player_turn
+
+    def getColor(self) :
+        return self.player_color
 
     def __str__(self) :
         return "HUMAN"
