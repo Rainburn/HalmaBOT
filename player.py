a6ddf0297e5bbb0b33e1d6155261161a0775eacc
Rainburn <cyber221168@yahoo.com>
2020-10-20 14:22:02 +0700

    remove timer from player

--- player.py
+++ player.py
@@ -1,5 +1,3 @@
-import time, datetime
-
 class Player :
 
     def __init__(self, color):
@@ -20,11 +18,5 @@ class Player :
         return "HUMAN"
 
 
-timer_stop = datetime.datetime.utcnow() + datetime.timedelta(seconds=5)
-while True :
-    x = input("input something :")
-    print(x)
-    if datetime.datetime.utcnow() > timer_stop :
-        print("Timer Complete")
-        break
+
 
